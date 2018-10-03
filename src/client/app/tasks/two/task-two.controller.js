af46d25621d14fd607792b72ccf938ea72ad9ab9
pistatic <pistatic@freemail.hu>
2018-10-03 18:25:21 +0200

    task 2 down

--- src/client/app/tasks/two/task-two.controller.js
+++ src/client/app/tasks/two/task-two.controller.js
@@ -10,7 +10,8 @@
     function TaskTwo(dataservice, logger) {
         var vm = this;
         vm.problem2Solved = problem2Solved;
-
+		vm.checkBoxes;
+		
         activate();
 
         function activate(){
@@ -22,6 +23,22 @@
         function problem2Solved(){
             logger.success('Hurray!', '');
         }
+		
+		vm.checkValidity = function(testForm){
+			var valid = false;
+			for(var a in vm.checkBoxes){
+				if(vm.checkBoxes[a].id==true) {
+					valid = true;
+					break;
+				}
+			}
+			testForm.checkBox.$valid = valid;
+			testForm.checkBox.$invalid = !valid;
+
+			testForm.dropDown.$valid = vm.dropDown!=undefined;
+			
+			testForm.$valid = testForm.checkBox.$valid && testForm.dropDown.$valid;
+		}
     }
 
 })();
